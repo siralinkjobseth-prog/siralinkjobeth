b5512ef0a00e62ea51ae75df093a316ed9c97eb3
siralinkjobseth-prog <siralinkjobs.eth@gmail.com>
2026-06-23 09:56:39 +0300

    Update constants.js

--- js/config/constants.js
+++ js/config/constants.js
@@ -1,1 +1,15 @@
+export const ROUTES = {
 
+SPLASH: "pages/splash.html",
+
+HOME: "pages/home.html",
+
+JOBS: "pages/jobs.html",
+
+PROFILE: "pages/profile.html",
+
+ABOUT: "pages/about.html"
+
+};
+
+export const ADMIN_ROLE = "super_admin";
