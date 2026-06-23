b8344e125ab55841b6acc2ba6d915904860517b6
siralinkjobseth-prog <siralinkjobs.eth@gmail.com>
2026-06-23 10:02:54 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
+import { authenticateUser }
+from "./auth/telegram-auth.js";
 
+(async () => {
+
+    const user =
+    await authenticateUser();
+
+    if(!user){
+
+        alert(
+        "Authentication Failed"
+        );
+
+        return;
+    }
+
+    setTimeout(() => {
+
+        window.location.href =
+        "pages/splash.html";
+
+    },1500);
+
+})();
