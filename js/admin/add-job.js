5dcd056791c8b542b59900445c2408e08c9e9b0c
siralinkjobseth-prog <siralinkjobs.eth@gmail.com>
2026-06-23 09:54:09 +0300

    Update add-job.js

--- js/admin/add-job.js
+++ js/admin/add-job.js
@@ -1,1 +1,34 @@
+import { supabase }
+from "../config/supabase.js";
 
+import {
+sendSmartNotification
+}
+from "./notifications.js";
+
+export async function addJob(job){
+
+const { data, error }
+=
+await supabase
+.from("jobs")
+.insert([job])
+.select()
+.single();
+
+if(error){
+
+alert(error.message);
+
+return;
+}
+
+await sendSmartNotification(
+data.id
+);
+
+alert(
+"Job Posted Successfully"
+);
+
+}
