345246d65e071ae4f684cb477dd0f59064c2136e
siralinkjobseth-prog <siralinkjobs.eth@gmail.com>
2026-06-23 09:54:56 +0300

    Update session.js

--- js/auth/session.js
+++ js/auth/session.js
@@ -1,1 +1,27 @@
+export function saveSession(user){
 
+    localStorage.setItem(
+        "user",
+        JSON.stringify(user)
+    );
+}
+
+export function getSession(){
+
+    return JSON.parse(
+        localStorage.getItem("user")
+    );
+}
+
+export function isLoggedIn(){
+
+    return !!localStorage.getItem("user");
+}
+
+export function logout(){
+
+    localStorage.removeItem("user");
+
+    window.location.href =
+    "../index.html";
+}
