cbc359589ada92a1f50e151189207f5267297550
siralinkjobseth-prog <siralinkjobs.eth@gmail.com>
2026-06-23 09:57:25 +0300

    Update dashboard.js

--- js/dashboard/dashboard.js
+++ js/dashboard/dashboard.js
@@ -1,1 +1,32 @@
+import { getSession }
+from "../auth/session.js";
 
+import { loadStatistics }
+from "./statistics.js";
+
+import { loadRecentJobs }
+from "./recent-jobs.js";
+
+const user = getSession();
+
+if(user){
+
+document.getElementById(
+"userName"
+).innerText =
+user.full_name ||
+user.username;
+
+if(user.profile_photo){
+
+document.getElementById(
+"userAvatar"
+).src =
+user.profile_photo;
+
+}
+
+}
+
+loadStatistics();
+loadRecentJobs();
